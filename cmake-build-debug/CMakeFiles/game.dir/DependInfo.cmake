f703b635f0d08af298b548b1539d1c29304a21a7
utec <marianocalderon21@homtail.com>
2019-06-21 16:50:49 -0500

    segunda act.

---
+++ cmake-build-debug/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/utec/Documentos/pfinal/Proyecto-2-POO20191/Controlador.cpp" "/home/utec/Documentos/pfinal/Proyecto-2-POO20191/cmake-build-debug/CMakeFiles/game.dir/Controlador.cpp.o"
+  "/home/utec/Documentos/pfinal/Proyecto-2-POO20191/Tierra.cpp" "/home/utec/Documentos/pfinal/Proyecto-2-POO20191/cmake-build-debug/CMakeFiles/game.dir/Tierra.cpp.o"
+  "/home/utec/Documentos/pfinal/Proyecto-2-POO20191/game.cpp" "/home/utec/Documentos/pfinal/Proyecto-2-POO20191/cmake-build-debug/CMakeFiles/game.dir/game.cpp.o"
+  "/home/utec/Documentos/pfinal/Proyecto-2-POO20191/objeto.cpp" "/home/utec/Documentos/pfinal/Proyecto-2-POO20191/cmake-build-debug/CMakeFiles/game.dir/objeto.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
