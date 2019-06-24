4a02bc4ec2a92f0e0b88282537b655b3b169034e
marianocs23 <39911704+marianocs23@users.noreply.github.com>
2019-06-24 10:48:51 -0500

    Update Controlador.h

--- Controlador.h
+++ Controlador.h
@@ -1,14 +1,28 @@
-//
-// Created by utec on 21/06/19.
-//
+#ifndef AGREGACION_MENU_H
+#define AGREGACION_MENU_H
 
-#ifndef GAME_CONTROLADOR_H
-#define GAME_CONTROLADOR_H
+#include "Tierra.h"
+#include "Tipos.h"
 
+template <typename T>
+T input(string label) {
+ T value;
+ cout << label;
+ cin >> value;
+ cin.clear();
+ cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+ return value;
+}
 
-class Controlador {
+/*
+//--- Esta seria la funcion para leer un string,
+//--- esta función si se quiere generalizar para colocar un label
+//--- y leer cualquier tipo de dato se tendría que convertir al template anterior
 
-};
-
-
-#endif //GAME_CONTROLADOR_H
+string&& input(string label) {
+ string value;
+ cout << label;
+ cin >> value;
+ return move(value);
+}
+*/
